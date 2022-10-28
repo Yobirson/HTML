4f57bce243f2f1eb4f680379d2cd8d79d664b010
Yobirson <115845685+Yobirson@users.noreply.github.com>
2022-10-28 17:02:07 -0500

    Update ejercicio.php

--- ejercicio001/ejercicio.php
+++ ejercicio001/ejercicio.php
@@ -1,11 +1,13 @@
 <?php
-#!/bin/sh
-n1=55
-n2=35
-resul=0
-echo introduce numero 1
-read n1
-echo introduce numero 2
-read n2
-resltado='expr n1+n2'
-echo el reultado es $resultado
+#!/bin/bash
+n1=45
+  n2=45
+  for ((i=1; i<=2; i++)); do
+    for j in "ls output-$i-*`; do
+        echo "$j"
+ 
+        metab=$(cat $j|grep EndBuffer|awk '{sum+=$2} END { print sum/120}') (line15)
+        num= $num + $metab   (line16)
+    done
+    echo "$num"
+ done
